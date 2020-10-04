14369a74cead1871433aea7eba8067a1c28488da
DmitryM <rf00rm@gmail.com>
2020-10-05 00:56:02 +0700

    Minor import change.
     - import principle changed to avoid annotated direct import.

--- src/bot/helpers/read_config.rs
+++ src/bot/helpers/read_config.rs
@@ -1,9 +1,9 @@
 use std::fs::File;
-
-#[path = "../models/bot_config.rs"]
-mod bot_config;
 extern crate serde_yaml;
-pub fn read_init_config(config_name: Option<String>) -> bot_config::BotConfig {
+
+use crate::bot::models::BotConfig;
+
+pub fn read_init_config(config_name: Option<String>) -> BotConfig {
     let name = match config_name {
         None => "config.yaml".to_string(),
         Some(result_str) => result_str.to_string(),
